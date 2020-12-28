68f7ed43ec19f6d30bdcd52365628b4783b3bec6
arendach <arendach.taras@gmail.com>
2020-12-28 13:54:33 +0200

    fix macros

--- src/MultiSessionsServiceProvider.php
+++ src/MultiSessionsServiceProvider.php
@@ -11,14 +11,10 @@ class MultiSessionsServiceProvider extends ServiceProvider
         $this->app->singleton(Session::class, function ($app) {
             return new Session();
         });
-
-        include __DIR__ . '/macro.php';
     }
 
     public function boot(): void
     {
-        include __DIR__ . '/macro.php';
-
         $this->publishes([
             __DIR__ . '/config/multisessions.php' => config_path('multisessions.php'),
         ], 'multisessions');
